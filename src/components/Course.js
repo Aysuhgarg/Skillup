8469684f807d315608f0402671fc081e987d9141
Aysuhgarg <coolayushgarg222@gmail.com>
2022-08-31 19:37:08 +0530

    Added Id in frontend

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import {Card,CardBody,CardSubtitle,CardTitle,CardText,Button,CardFooter,Container} from "reactstrap";
+import {Card,CardBody,CardSubtitle,CardTitle,CardText,Button,CardFooter,Container, Row,Col} from "reactstrap";
 //import Card from 'react-bootstrap/Card'
 
 const Course=({course})=>{
 
     return (
         <Card className='text-center'>
-            <CardBody>
-                <CardSubtitle className='font-weight-bold' ><h5>{course.title}</h5></CardSubtitle>
-                <CardText>It a {course.name} course for beginers</CardText>
-                <Container className='text-center'>
-                    <Button color= "danger">Delete</Button>
-                    <Button color="warning ml-3">Update</Button>
-                </Container>
-
-            </CardBody>
+            <Container>
+                {/* <Row >
+                    <Col md={2}  className=''>
+                        <CardBody className='text-center mt-4'><h1>{course.id}</h1></CardBody>
+                    </Col>
+                    <Col md={10} className='bg-warning'> */}
+                <CardBody>
+                    <CardSubtitle className='font-weight-bold' ><h5>{course.name}</h5></CardSubtitle>
+                    <CardText>{course.title}</CardText>
+                    <Container className='text-center'>
+                        <Button color= "danger">Delete</Button>
+                        <Button color="primary ml-3">Update</Button>
+                    </Container>
+                </CardBody>
+                {/* </Col>
+                </Row> */}
+           </Container>
         </Card>
     );
 };
